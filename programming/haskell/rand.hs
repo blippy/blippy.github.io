95e7cefc251b962a1f574e1ab008b1e2600b2c96
Mark Carter <alt.mcarter@gmail.com>
2018-02-10 22:45:57 +0000

    Major additions

---
+++ programming/haskell/rand.hs
@@ -0,0 +1,23 @@
+-- GENERATING RANDOM NUMBERS
+
+import Data.List
+import System.Random
+
+-- some definitions
+
+-- auxillary function
+randomlist :: Int -> StdGen -> [Float]
+randomlist n = take n . unfoldr (Just . random)
+
+-- generate a list of floats
+randfs :: Int -> IO [Float]
+randfs n = do
+  seed  <- newStdGen
+  return $ randomlist n seed
+
+-- example usage
+main = do
+  rands <- randfs 10
+  print rands
+  print $ sum rands
+
